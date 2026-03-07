c71b56f8afa78774bcf36fec82a7de0d49884176
EC2 Default User <ec2-user@ip-172-31-48-171.ap-south-1.compute.internal>
2026-03-07 09:19:02 +0000

    Test for presentation

--- app.py
+++ app.py
@@ -10,16 +10,17 @@ app = Flask(__name__)
 
 REQUEST_COUNT = 0
 
+APP_VERSION = os.getenv("APP_VERSION", "local-dev")
+
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_NAME = os.getenv("DB_NAME", "appdb")
 DB_USER = os.getenv("DB_USER", "admin")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "admin123")
+
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 
-# Redis connection
 redis_client = redis.Redis(host=REDIS_HOST, port=6379, decode_responses=True)
 
-# PostgreSQL connection pooling
 db_pool = pool.SimpleConnectionPool(
     1, 5,
     host=DB_HOST,
@@ -28,15 +29,19 @@ db_pool = pool.SimpleConnectionPool(
     password=DB_PASSWORD
 )
 
+
 def get_db_connection():
     return db_pool.getconn()
+
 
 def release_db_connection(conn):
     db_pool.putconn(conn)
 
+
 def init_db():
     conn = get_db_connection()
     cur = conn.cursor()
+
     cur.execute("""
         CREATE TABLE IF NOT EXISTS visits (
             id SERIAL PRIMARY KEY,
@@ -44,54 +49,72 @@ def init_db():
             visited_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         );
     """)
+
     conn.commit()
-    conn.close()
+    release_db_connection(conn)
+
 
 @app.route("/")
 def home():
     global REQUEST_COUNT
     REQUEST_COUNT += 1
+
     return render_template(
         "index.html",
         hostname=socket.gethostname(),
-	"CI/CD check"
-        request_count=REQUEST_COUNT
+        request_count=REQUEST_COUNT,
+        version=APP_VERSION
     )
+
 
 @app.route("/health")
 def health():
-    return jsonify(status="healthy", container=socket.gethostname())
+    return jsonify(
+        status="healthy",
+        container=socket.gethostname(),
+        version=APP_VERSION
+    )
+
 
 @app.route("/api/info")
 def info():
     return jsonify(
         hostname=socket.gethostname(),
-        requests_served=REQUEST_COUNT
+        requests_served=REQUEST_COUNT,
+        version=APP_VERSION
     )
+
 
 @app.route("/api/db")
 def db_check():
+
     cached = redis_client.get("db_version")
+
     if cached:
         return jsonify(database_version=cached, source="cache")
 
     try:
         conn = get_db_connection()
         cur = conn.cursor()
+
         cur.execute("SELECT version();")
         version = cur.fetchone()[0]
+
         release_db_connection(conn)
 
         redis_client.setex("db_version", 30, version)
+
         return jsonify(database_version=version, source="database")
 
     except Exception as e:
         return jsonify(error=str(e)), 500
 
+
 @app.route("/api/load")
 def simulate_load():
     time.sleep(3)
     return jsonify(message="Load simulation complete")
+
 
 if __name__ == "__main__":
     init_db()
